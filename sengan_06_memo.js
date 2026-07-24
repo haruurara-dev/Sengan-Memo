bdb18ccfd0feef5f52cf748b463a3e112b8afa99
haruurara-dev <badcr111parabola@gmail.com>
2026-07-24 20:20:23 +0900

    Add files via upload

---
+++ sengan_06_memo.js
@@ -0,0 +1,28 @@
+const morningMemo=document.getElementById("morningMemo");
+const saveMorning=document.getElementById("saveMorning");
+const backButton=document.getElementById("backButton");
+
+const nightMemo=document.getElementById("nightMemo");
+const saveNight=document.getElementById("saveNight");
+
+if (morningMemo){
+    morningMemo.value=localStorage.getItem("morningMemo") ||"";
+    saveMorning.addEventListener("click", function(){
+        localStorage.setItem("morningMemo", morningMemo.value);
+        alert("朝洗顔メモを保存しました。")
+    });
+    backButton.addEventListener("click", function(){
+        location.href="index.html";
+    });
+}
+
+if (nightMemo){
+    nightMemo.value=localStorage.getItem("nightMemo") ||"";
+    saveNight.addEventListener("click", function(){
+        localStorage.setItem("nightMemo", nightMemo.value);
+        alert("夜洗顔メモを保存しました。")
+    });
+    backButton.addEventListener("click", function(){
+        location.href="index.html";
+    });
+}
