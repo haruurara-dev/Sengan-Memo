3f467fecb37420b0350c3065f6831121980392c9
haruurara-dev <badcr111parabola@gmail.com>
2026-07-24 19:27:55 +0900

    Add files via upload

---
+++ sengan_05_index.js
@@ -0,0 +1,10 @@
+const morningButton = document.getElementById("morning-btn");
+const nightButton = document.getElementById("night-btn");
+
+morningButton.addEventListener("click", function(){
+    location.href="sengan_02_morning.html";
+});
+
+nightButton.addEventListener("click", function(){
+    location.href="sengan_03_night.html";
+});
